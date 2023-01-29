c600ffef04ef191294be375bb8b4f38a80e89942
SayantanStackflow <sayantan.college6291@gmail.com>
2023-01-29 20:31:21 +0000

    Eudoxus Sans

--- mindverse/app/layout.js
+++ mindverse/app/layout.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 export default function RootLayout({ children }) {
   return (
@@ -7,8 +7,11 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
