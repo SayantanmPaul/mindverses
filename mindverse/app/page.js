ebb45cdc7dd81355575afc6c963001bfc847a967
SayantanStackflow <sayantan.college6291@gmail.com>
2023-01-29 10:16:40 +0000

    added starter files

---
+++ mindverse/app/page.js
@@ -0,0 +1,27 @@
+import Footer from '@/components/footer';
+import Navbar from '@/components/Navber';
+import About from '@/sections/about';
+import Hero from '@/sections/Hero';
+import Explore from '@/sections/Explore';
+import GetStarted from '@/sections/GetStarted';
+import WhatsNew from '@/sections/WhatsNew';
+import World from '@/sections/World';
+import Insights from '@/sections/Insights';
+import Feedback from '@/sections/Feedback';
+
+const Page = () => (
+  <div>
+    <Navbar />
+    <Hero />
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default Page;
